d32e56d55e09085a4e0e49181db127d47811f9ac
CodeWizardMagic <230017@astanait.edu.kz>
2024-10-06 23:28:11 +0500

    1,2,3 patterns
    Adding Singletone, Factory Method, Abstract Factory

---
+++ Blue.java
@@ -0,0 +1,6 @@
+class Blue implements Color {
+    @Override
+    public void fill() {
+        System.out.println("Filling with Blue color");
+    }
+}
---
+++ ShapeFactory.java
@@ -0,0 +1,3 @@
+abstract class ShapeFactory {
+    public abstract Shape createShape();
+}
